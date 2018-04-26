780747ac710c64b181099329c9a494c4d529897b
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-04-26 11:02:25 +0200

    Refactoring and added FindBugs and CheckStyle plugins

---
+++ src/main/java/it/unibo/ci/niklegend/HelloWorld.java
@@ -0,0 +1,13 @@
+package it.unibo.ci.niklegend;
+
+
+public final class HelloWorld {
+
+    public static void main(final String... args) {
+        System.out.println("Hello World!");
+    }
+
+    private HelloWorld() {
+    }
+
+}
--- src/main/java/piscaglia/Launcher.java
+++
@@ -1,7 +0,0 @@
-package piscaglia;
-
-public class Launcher {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
