621842411326e7d50aa14281fa5ba8a8105c0bb7
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-04-26 09:57:06 +0200

    Testing build system...

--- src/main/java/it/unibo/ci/mvandi/HelloWorld.java
+++ src/main/java/it/unibo/ci/mvandi/HelloWorld.java
@@ -3,7 +3,7 @@ package it.unibo.ci.mvandi;
 public final class HelloWorld {
 
     public static void main(final String... args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 
     private HelloWorld() {
