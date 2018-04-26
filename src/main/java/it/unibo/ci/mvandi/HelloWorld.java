06fbbe444ee56c82e0578ddca0697cc2866aa661
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-04-26 09:52:54 +0200

    Added hello world example and configured Travis CI build system

---
+++ src/main/java/it/unibo/ci/mvandi/HelloWorld.java
@@ -0,0 +1,12 @@
+package it.unibo.ci.mvandi;
+
+public final class HelloWorld {
+
+    public static void main(final String... args) {
+        System.out.println("Hello, World!");
+    }
+
+    private HelloWorld() {
+    }
+
+}
